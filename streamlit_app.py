045e75233f45553c0cfaa2c79fd1a0bb0a2bb032
GabrielRenno <gabriel_renno@outlook.com>
2023-10-06 17:10:50 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ try:
 except FileNotFoundError:
     conversation_log = pd.DataFrame(columns=['Email', 'User Message', 'System Answer', 'Time'])
 
-# Authentication function (assuming user_data is defined somewhere in your code)
+# Authentication function
 def authenticate_user(email, password):
     if email in user_data:
         return user_data[email] == password
@@ -158,6 +158,9 @@ def display_main_page(email):
     st.title("Col-legi Sant Miquel Chatbot")
     st.write("**Welcome to the Col-legi Sant Miquel Chatbot test App. Ask a question, and the chatbot will reply. The chatbot uses GPT-4 to answer questions about Col-legi Sant Miquel in Barcelona. This is the first version in test.**")
 
+    # Reverse the order of the conversation log
+    reversed_log = conversation_log[conversation_log['Email'] == email].iloc[::-1]
+    
     # Style for the conversation log
     conversation_style = """
         <style>
@@ -179,23 +182,20 @@ def display_main_page(email):
 
     st.markdown(conversation_style, unsafe_allow_html=True)
 
+    for index, row in reversed_log.iterrows():
+        st.markdown(f"<div class='conversation-log'><span class='user-message'>You:</span> {row['User Message']}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div class='conversation-log'><span class='bot-message'>Chatbot:</span> {row['System Answer']}</div>", unsafe_allow_html=True)
+
     # Text box at the bottom of the page for user input
     question = st.text_area("Ask a question:", key='question_input', height=100, max_chars=500)
 
     if st.button("Ask"):
         answer = run_agent(agent, question)
+        st.write("***You:***", question)
+        st.write("***Chatbot:***", answer)
 
         if not is_duplicate_conversation(email, question, answer):
             conversation_log.loc[len(conversation_log)] = [email, question, answer, datetime.utcnow()]
-
-        #st.write("***You:***", question)
-        #st.write("***Chatbot:***", answer)
-
-    # Display the conversation log
-    for index, row in conversation_log.iterrows():
-        if row['Email'] == email:
-            st.markdown(f"<div class='conversation-log'><span class='user-message'>You:</span> {row['User Message']}</div>", unsafe_allow_html=True)
-            st.markdown(f"<div class='conversation-log'><span class='bot-message'>Chatbot:</span> {row['System Answer']}</div>", unsafe_allow_html=True)
 
     # Save conversation log as a csv file
     conversation_log.to_csv(conversation_log_file, index=False)
@@ -228,6 +228,7 @@ elif 'email' in st.session_state:
 
 else:
     st.error("Please log in to continue.")
+
 
 
 
